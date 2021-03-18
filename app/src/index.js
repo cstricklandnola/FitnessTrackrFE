68b7a5dca86dd2e7bb50fa1cf2a98c577033d51d
Ryan Richoux <Ryan.Richoux@Gmail.com>
2021-03-17 19:02:49 -0500

    Login/Register

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
+import {
+    Login,
+    LogOut,
+    Register
+    
+    
+  } from "./components"
+
+
+function App() {
+   
+  const [authorized, setAuthorized] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
+  
+  return (
+    <Router>
+      {/* <Header /> */}
+            <div>
+        <nav>
+          <ul>
+            {!authorized ? (
+              <li>
+                <Link to="/Register">Register</Link>
+              </li>
+            ) : null}
+            {!authorized ? (
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
+            ) : null}
+            {authorized ? (
+              <li>
+                <Link to="/LogOut">LogOut</Link>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+
+        <Switch>
+          
+          <Route path="/Register">
+            {!authorized ? <Register setAuthorized={setAuthorized} /> : null}
+          </Route>
+          <Route path="/Login">
+            {!authorized ? (
+              <Login
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+                setAuthorized={setAuthorized}
+                authorized={authorized}
+              />
+            ) : null}
+          </Route>
+          
+          <Route path="/LogOut">
+            <LogOut
+              setCurrentUser={setCurrentUser}
+              setAuthorized={setAuthorized}
+              authorized={authorized}
+            />
+          </Route>
+          
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
 
 ReactDOM.render(
     <App />,
