402b36973dcd0a5ea78e4807d6b9e08819344a3a
Corey Frederickson <coreyfrederickson@Coreys-MacBook-Pro.local>
2021-03-17 19:15:37 -0500

    Testing runs

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router"
-
+import { BrowserRouter as Router, Link } from "react-router-dom"
+import {Switch, Route} from "react-router"
 
 
 function App() {
